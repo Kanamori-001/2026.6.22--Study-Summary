7de01be9815fc27c09b31844bc138250cdf2ebb0
Kanamori-001 <y.kanamori.job@gmail.com>
2026-06-29 08:47:06 +0900

    コメントアウトを追加

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
 const pageTopBtn = document.getElementById("page-top");
+//対象はページトップのボタン
 
 pageTopBtn.setAttribute("class",window.scrollY>50 ?"show" :"hide");
+//ページ表示時の動作を指定
 
 window.addEventListener("scroll", () => {
   pageTopBtn.setAttribute("class",window.scrollY>50 ?"show" :"hide");
 })
+//スクロールした際のボタンの挙動を設定。if文を"？"と"："で省略
 
 pageTopBtn.addEventListener("click",()=>{
   window.scrollTo({
     top:0, 
     behavior:"smooth"});
   });
+//ボタンをクリックした際の挙動を設定。"smooth"で挙動を滑らかに
